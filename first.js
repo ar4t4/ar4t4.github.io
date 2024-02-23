f8638bb90ece9e2a5678915f5892c67a7683ac76
asif <asifrahman00010@gmail.com>
2024-02-24 01:17:56 +0600

    design done?

--- first.js
+++ first.js
@@ -138,27 +138,27 @@ skillsHeader.forEach((el) => {
 
 
 /*======================= Services Modal ===================*/
-const modalViews = document.querySelectorAll(".services__modal"),
-  modalBtns = document.querySelectorAll(".services__button"),
-  modalCloses = document.querySelectorAll(".services__modal-close");
+// const modalViews = document.querySelectorAll(".services__modal"),
+//   modalBtns = document.querySelectorAll(".services__button"),
+//   modalCloses = document.querySelectorAll(".services__modal-close");
 
-let modal = function (modalClick) {
-  modalViews[modalClick].classList.add("active-modal");
-};
+// let modal = function (modalClick) {
+//   modalViews[modalClick].classList.add("active-modal");
+// };
 
-modalBtns.forEach((modalBtn, i) => {
-  modalBtn.addEventListener("click", () => {
-    modal(i);
-  });
-});
+// modalBtns.forEach((modalBtn, i) => {
+//   modalBtn.addEventListener("click", () => {
+//     modal(i);
+//   });
+// });
 
-modalCloses.forEach((modalClose) => {
-  modalClose.addEventListener("click", () => {
-    modalViews.forEach((modalView) => {
-      modalView.classList.remove("active-modal");
-    });
-  });
-});
+// modalCloses.forEach((modalClose) => {
+//   modalClose.addEventListener("click", () => {
+//     modalViews.forEach((modalView) => {
+//       modalView.classList.remove("active-modal");
+//     });
+//   });
+// });
 
 
 
@@ -168,19 +168,19 @@ modalCloses.forEach((modalClose) => {
 
 
 /*======================= Portfolio Swiper ===================*/
-// var swiper = new Swiper(".portfolio__container", {
-//   cssMode: true,
-//   loop: true,
+var swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
 
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-// });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]");
